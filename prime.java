5adf88c822a71fde5b18c2211e5e93f0bb0f8728
sameeksha509485 <22h51a0509@cmrcet.ac.in>
2024-11-28 14:48:15 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class prime {
+public class prime1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
